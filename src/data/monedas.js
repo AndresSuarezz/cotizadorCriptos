a152fd96b541fbdd2381f888732bf89ee2f94015
AndresSuarezz <anfesuguito22@gmail.com>
2023-07-31 22:17:40 -0500

    Proyecto Finalizado

---
+++ src/data/monedas.js
@@ -0,0 +1,8 @@
+const monedas = [
+  { id: "USD", nombre: "Dolar de estados unidos" },
+  { id: "COP", nombre: "Peso Colombiano" },
+  { id: "EUR", nombre: "Euro" },
+  { id: "GBP", nombre: "Libra esterlina" },
+];
+
+export {monedas}
